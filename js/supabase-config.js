d408f20008e736219eb80c2bf2b085b1de2c60f2
roopamathew <roopamathew586@gmail.com>
2026-08-30 14:31:35 +0530

    Update Supabase URL to remove endpoint path

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,3 +1,3 @@
-window.LEXORA_SUPABASE_URL = "https://wasoourrniamhzcixagt.supabase.co/rest/v1/";
+window.LEXORA_SUPABASE_URL = "https://wasoourrniamhzcixagt.supabase.co";
 
 window.LEXORA_SUPABASE_ANON_KEY = "sb_publishable_dDi_y4CLJDr2TKqmzMUYCQ_FYqSmGsV";
