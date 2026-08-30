3437726376f9460d99eb40085db9b6484409ef1f
roopamathew <roopamathew586@gmail.com>
2026-08-30 14:40:23 +0530

    Refactor Supabase configuration for global access

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,14 +1,3 @@
-(function () {
-  "use strict";
+window.LEXORA_SUPABASE_URL = "https://wasoourrniamhzcixagt.supabase.co";
 
-  window.LEXORA_SUPABASE_URL =
-    "https://wasoourrniamhzcixagt.supabase.co";
-
-  window.LEXORA_SUPABASE_ANON_KEY =
-    "sb_publishable_dDi_y4CLJDr2TKqmzMUYCQ_FYqSmGsV";
-
-  console.log(
-    "Lexora Supabase URL:",
-    window.LEXORA_SUPABASE_URL
-  );
-})();
+window.LEXORA_SUPABASE_ANON_KEY = "sb_publishable_3iqaSn5I7yEkWcl2GOKOIg_zq954raL";
