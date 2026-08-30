d0f6fa3075cabfd286ad5543c4d38e94351bde6a
roopamathew <roopamathew586@gmail.com>
2026-08-30 14:34:40 +0530

    Refactor Supabase config with IIFE and logging
    Refactor Supabase configuration to use an IIFE and log the URL.

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,3 +1,14 @@
-window.LEXORA_SUPABASE_URL = "https://wasoourrniamhzcixagt.supabase.co";
+(function () {
+  "use strict";
 
-window.LEXORA_SUPABASE_ANON_KEY = "sb_publishable_dDi_y4CLJDr2TKqmzMUYCQ_FYqSmGsV";
+  window.LEXORA_SUPABASE_URL =
+    "https://wasoourrniamhzcixagt.supabase.co";
+
+  window.LEXORA_SUPABASE_ANON_KEY =
+    "sb_publishable_dDi_y4CLJDr2TKqmzMUYCQ_FYqSmGsV";
+
+  console.log(
+    "Lexora Supabase URL:",
+    window.LEXORA_SUPABASE_URL
+  );
+})();
